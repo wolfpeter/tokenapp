5e5fd641fc5fee924d8f76defb7e005df0a577a5
Peter Wolf <wolf.peter@wopi.hu>
2024-10-13 21:59:15 +0200

    Initial commit

---
+++ TokenApp/App.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System.Windows;
+using Application = System.Windows.Application;
+using MessageBox = System.Windows.Forms.MessageBox;
+
+namespace TokenApp;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
+{
+    private static Mutex mutex = null;
+
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        const string mutexName = "MyUniqueAppName";
+
+        // Ellenőrizzük, hogy van-e már egy példány futásban
+        bool isNewInstance;
+        mutex = new Mutex(true, mutexName, out isNewInstance);
+
+        if (!isNewInstance)
+        {
+            MessageBox.Show("Az alkalmazás már fut!");
+            Application.Current.Shutdown();  // Az új példány bezárása
+            return;
+        }
+
+        base.OnStartup(e);
+    }
+}
---
+++ TokenApp/Views/MainWindow.xaml.cs
@@ -0,0 +1,170 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Threading;
+
+namespace TokenApp.Views;
+
+public partial class MainWindow : Window
+{
+    private NotifyIcon notifyIcon;
+    private DispatcherTimer _timer;
+    private int _timeLeft;
+    private Random _random;
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        _random = new Random();
+        StartTokenTimer();
+        
+        CreateTrayIcon();
+    }
+    
+    private void CreateTrayIcon()
+    {
+        notifyIcon = new NotifyIcon();
+        notifyIcon.Icon = SystemIcons.Application; //TODO Icon file!
+        notifyIcon.Visible = true;
+        notifyIcon.DoubleClick += NotifyIcon_DoubleClick;
+    }
+    
+    private void NotifyIcon_DoubleClick(object sender, EventArgs e)
+    {
+        Show();
+        WindowState = WindowState.Normal;
+        Activate();
+    }
+    
+    private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+    {
+        // Megakadályozza az ablak bezárását, csak minimalizáljuk
+        e.Cancel = true;
+        Hide();
+    }
+
+    private void StartTokenTimer()
+    {
+        _timeLeft = 6000; // 60 másodperc visszaszámlálás
+        TokenProgressBar.Value = _timeLeft;
+
+        _timer = new DispatcherTimer();
+        _timer.Interval = TimeSpan.FromMilliseconds(10);
+        _timer.Tick += Timer_Tick;
+        _timer.Start();
+    }
+
+    private void Timer_Tick(object sender, EventArgs e)
+    {
+        _timeLeft--;
+        TokenProgressBar.Value = _timeLeft;
+
+        if (_timeLeft <= 0)
+        {
+            GenerateNewToken();
+        }
+    }
+
+    private void GenerateNewToken()
+    {
+        _timeLeft = 6000;
+        string newToken = GenerateRandomToken();
+        TokenCode.Text = newToken;
+    }
+
+    private void EditButton_Click(object sender, RoutedEventArgs e)
+    {
+        PrinterNameTxt.Visibility = Visibility.Collapsed;
+        PrinterNameComboBox.Visibility = Visibility.Visible;
+    }
+
+    private void PrinterSelectionChanged(object sender, SelectionChangedEventArgs e)
+    {
+        PrinterNameTxt.Visibility = Visibility.Visible;
+        PrinterNameComboBox.Visibility = Visibility.Collapsed;
+    }
+
+    private string GenerateRandomToken()
+    {
+        // Generálj egy új token kódot, például 3 betű + 3 szám formátumban
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        const string nums = "0123456789";
+        string token = new string(new char[] {
+            chars[_random.Next(chars.Length)],
+            chars[_random.Next(chars.Length)],
+            chars[_random.Next(chars.Length)],
+            nums[_random.Next(nums.Length)],
+            nums[_random.Next(nums.Length)],
+            nums[_random.Next(nums.Length)]
+        });
+        return token;
+    }
+
+    private void ApiRootPageNext(object sender, RoutedEventArgs e)
+    {
+        SwitchToLoginPage();
+    }
+    
+    private void LoginPageNext(object sender, RoutedEventArgs e)
+    {
+        SwitchToSecondaryPasswordPage();
+    }
+    
+    private void SecondaryPasswordPageNext(object sender, RoutedEventArgs e)
+    {
+        SwitchToAliasPage();
+    }
+    
+    private void AliasPageNext(object sender, RoutedEventArgs e)
+    {
+        SwitchToSettings(sender, e);
+    }
+    
+    private void SwitchToSettings(object sender, RoutedEventArgs e)
+    {
+        CloseAllGridsVisibility();
+        SettingsPage.Visibility = Visibility.Visible;
+    }
+
+    private void SwitchToMainPage(object sender, RoutedEventArgs e)
+    {
+        GenerateNewToken();
+        
+        CloseAllGridsVisibility();
+        MainPage.Visibility = Visibility.Visible;
+    }
+    
+    private void SwitchToApiRootPage()
+    {
+        CloseAllGridsVisibility();
+        ApiRootPage.Visibility = Visibility.Visible;
+    }
+    
+    private void SwitchToLoginPage()
+    {
+        CloseAllGridsVisibility();
+        LoginPage.Visibility = Visibility.Visible;
+    }
+    
+    private void SwitchToSecondaryPasswordPage()
+    {
+        CloseAllGridsVisibility();
+        SecondPasswordPage.Visibility = Visibility.Visible;
+    }
+    
+    private void SwitchToAliasPage()
+    {
+        CloseAllGridsVisibility();
+        AliasPage.Visibility = Visibility.Visible;
+    }
+    
+    private void CloseAllGridsVisibility()
+    {
+        foreach (var child in ParentGrid.Children)
+        {
+            if (child is Grid grid)
+            {
+                grid.Visibility = Visibility.Collapsed;
+            }
+        }
+    }
+}
